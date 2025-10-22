a20473b7fa4cbd79a593f9d9ac18fb4ee8e39e6f
lxd <648612680@qq.com>
2025-10-22 15:58:48 +0800

    ``` feat(vite-config): add server port configuration and alias for vue apps
    - Set server port to 9528 for microAppVue
    - Set server port to 9527 for vue-main app
    - Add alias '@' pointing to src directory for vue-main
    - Integrate router into vue-main app
    ```

--- apps/vue-main/vite.config.js
+++ apps/vue-main/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  alias: {
+    '@': resolve(__dirname, 'src')
+  },
+  server: {
+    port: 9527,
+  }
 })
